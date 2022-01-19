83a737da8c4af6123ac9ce1d439e8655c6bb7b52
Aleksey <lexa.shvetsoff@yandex.ru>
2022-01-19 21:30:55 +0800

    Отфильтрована база данных

--- main.py
+++ main.py
@@ -11,12 +11,8 @@ if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
 
-passcards = Passcard.objects.all()
+    # passcards = Passcard.objects.all()
 
-active_passcards = []
-for passcard in passcards:
-    # print(passcard.owner_name, passcard.passcode, passcard.created_at, passcard.is_active, sep='\n')
-    if passcard.is_active:
-        active_passcards.append(passcard)
+    active_passcards = Passcard.objects.filter(is_active=True)
 
-print(f'Всего пропусков: {Passcard.objects.count()}', f'Активных пропусков: {len(active_passcards)}')
+    print(f'Активных пропусков: {len(active_passcards)}')
