ed15ce6fad2c6f8fb2105422c473dff8c104f48e
Aleksey <lexa.shvetsoff@yandex.ru>
2022-01-19 20:58:14 +0800

    Получены карточки прпопусков

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+passcard = Passcard.objects.all()
+print(passcard)
