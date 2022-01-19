cedf39666b5fb9bb80dfcb0d7d9db861dfc8ecaf
Aleksey <lexa.shvetsoff@yandex.ru>
2022-01-19 22:00:48 +0800

    Обновлены исходные файлы

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import os
 
-import django
+from django.core.management import execute_from_command_line
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-django.setup()
-
-from datacenter.models import Passcard  # noqa: E402
-
-if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-
-    # passcards = Passcard.objects.all()
-
-    active_passcards = Passcard.objects.filter(is_active=True)
-
-    print(f'Активных пропусков: {len(active_passcards)}')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
