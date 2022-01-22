b649362c85fde9669ba435a08a2280ab7d94c3ed
Aleksey <lexa.shvetsoff@yandex.ru>
2022-01-22 22:36:50 +0800

    Вывод людей в хранилище на странице

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,46 @@
+import datetime
+
+from django.utils.timezone import localtime
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
+def get_datetime(localtime):
+    return datetime.datetime(localtime.year,
+                            localtime.month,
+                            localtime.day,
+                            localtime.hour,
+                            localtime.minute,
+                            localtime.second)
+
+
+def get_duration(visit):
+    now = get_datetime(localtime())
+    then = get_datetime(visit.entered_at)
+    return now - then
+
+
+def format_duration(duration):
+    hours = int(duration.total_seconds() // 3600)
+    minutes = int((duration.total_seconds() % 3600) // 60)
+    return f'{hours} ч. {minutes} мин.'
+
+
 def storage_information_view(request):
     # Программируем здесь
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    not_leave_visits = Visit.objects.filter(leaved_at=None)
+
+    non_closed_visits = []
+    for not_leave_visit in not_leave_visits:
+        duration = get_duration(not_leave_visit)
+        non_closed_visit = {'who_entered': not_leave_visit.passcard,
+                            'entered_at': localtime(not_leave_visit.entered_at),
+                            'duration': format_duration(duration),
+                           }
+        non_closed_visits.append(non_closed_visit)
+
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
