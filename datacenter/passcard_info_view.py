2bb4787040b61ea95502ee331322c39b6242d8c1
Aleksey <lexa.shvetsoff@yandex.ru>
2022-02-05 21:15:20 +0800

    Убрана копипаста кода

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,46 +1,9 @@
-import datetime
+from datacenter import functions as fn
 
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
-
-
-def get_datetime(localtime):
-    return datetime.datetime(localtime.year,
-                            localtime.month,
-                            localtime.day,
-                            localtime.hour,
-                            localtime.minute,
-                            localtime.second)
-
-
-def get_duration(visit):
-    if visit.leaved_at:
-        start = get_datetime(visit.entered_at)
-        closed = get_datetime(visit.leaved_at)
-        visit_time = closed - start
-    else:
-        now = get_datetime(localtime())
-        then = get_datetime(visit.entered_at)
-        visit_time = now - then
-    return visit_time
-
-
-def format_duration(duration):
-    hours = int(duration.total_seconds() // 3600)
-    minutes = int((duration.total_seconds() % 3600) // 60)
-    return f'{hours} ч. {minutes} мин.'
-
-
-def get_visit_time(visit):
-    # возвращает время в минутах
-    visit_time = get_duration(visit)
-    return int(visit_time.total_seconds() // 60)
-
-
-def is_visit_long(visit, minutes=60):
-    return get_visit_time(visit) >= minutes
 
 
 def passcard_info_view(request, passcode):
@@ -50,11 +13,11 @@ def passcard_info_view(request, passcode):
 
     this_passcard_visits = []
     for passcard_visit in passcard_visits:
-        duration = get_duration(passcard_visit)
+        duration = fn.get_duration(passcard_visit)
         visit = {
                     'entered_at': passcard_visit.entered_at,
-                    'duration': format_duration(duration),
-                    'is_strange': is_visit_long(passcard_visit),
+                    'duration': fn.format_duration(duration),
+                    'is_strange': fn.is_visit_long(passcard_visit),
                 }
         this_passcard_visits.append(visit)
 
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,30 +1,9 @@
-import datetime
+from datacenter import functions as fn
 
 from django.utils.timezone import localtime
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
-
-def get_datetime(localtime):
-    return datetime.datetime(localtime.year,
-                            localtime.month,
-                            localtime.day,
-                            localtime.hour,
-                            localtime.minute,
-                            localtime.second)
-
-
-def get_duration(visit):
-    now = get_datetime(localtime())
-    then = get_datetime(visit.entered_at)
-    return now - then
-
-
-def format_duration(duration):
-    hours = int(duration.total_seconds() // 3600)
-    minutes = int((duration.total_seconds() % 3600) // 60)
-    return f'{hours} ч. {minutes} мин.'
 
 
 def storage_information_view(request):
@@ -33,10 +12,10 @@ def storage_information_view(request):
 
     non_closed_visits = []
     for not_leave_visit in not_leave_visits:
-        duration = get_duration(not_leave_visit)
+        duration = fn.get_duration(not_leave_visit)
         non_closed_visit = {'who_entered': not_leave_visit.passcard,
                             'entered_at': localtime(not_leave_visit.entered_at),
-                            'duration': format_duration(duration),
+                            'duration': fn.format_duration(duration),
                            }
         non_closed_visits.append(non_closed_visit)
 
